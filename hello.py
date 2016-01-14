6706a1595f0648576ac0a0c5830d789a0760c0a6
shuroq <Cherry@Shuroqs-MacBook-Pro.local>
2016-01-13 20:05:51 -0500

    step 10, 11

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # replace the word 'hector' with your own name
 
 def hello_world():
-	return 'hello world, my name is shuroq’
+	return 'hello world, my name is ’
 
-print( hello_world() + 'hector' )
+print( hello_world() + ‘shuroq’ )
